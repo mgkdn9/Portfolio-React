6ddead80b02ea6cf3e5b6891e86892584eafc1c8
Mike Kohlberg <mkohlberg95@gmail.com>
2025-04-07 16:11:52 -0600

    fixed arrows

--- src/components/CarouselBtnNext.js
+++ src/components/CarouselBtnNext.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function ProjectCard(props) {
   if(props.index!==4)
   return (
-    <button className="carousel__button carousel__button--right">
-      <img src="https://i.imgur.com/p8SFtmi.png" alt="chevronR" />
+    <button className="carousel__button carousel__button--right" aria-label="Next">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <polyline points="9 18 15 12 9 6" />
+      </svg>
     </button>
   );
   return (<></>)
--- src/components/CarouselBtnPrev.js
+++ src/components/CarouselBtnPrev.js
@@ -3,9 +3,12 @@ import React from "react";
 export default function ProjectCard(props) {
   if(props.index!==0)
   return (
-    <button className="carousel__button carousel__button--left">
-      <img src="https://i.imgur.com/UiUhUQW.png" alt="chevronL" />
+    <button className="carousel__button carousel__button--left" aria-label="Previous">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <polyline points="15 18 9 12 15 6" />
+      </svg>
     </button>
+
   );
   return (<></>)
 }
